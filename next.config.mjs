5835ef40ebf27911eb42ddb04f19e7c47a9e1920
Bishal-laha <bishallaha12345@gmail.com>
2024-03-29 01:06:09 +0530

    last change

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // output:"export",
-    // //THIS IS TO CONFIG hostname "images.unsplash.com" is not configured PROBLEM
-    // images:{
-    //     domains:['images.unsplash.com']
-    // }
-    exportPathMap: async function () {
-    return {
-      '/': { page: '/' },
-      '/about': { page: '/about' },
-      '/contact':{page:'/contact'}
-      // Add custom routes here
-    };
-  },
-  export: true
+    output:"export",
+    //THIS IS TO CONFIG hostname "images.unsplash.com" is not configured PROBLEM
+    images:{
+        domains:['images.unsplash.com']
+    }
+//      exportPathMap: async function () {
+//     return {
+//       '/': { page: '/' },
+//       '/about': { page: '/about' },
+//       // Add custom routes here
+//     };
+//   },
+//   export: true
 };
 
 export default nextConfig;
